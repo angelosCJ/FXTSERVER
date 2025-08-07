f0b43fc3faf84afef17c993859ee763fd83f80cf
angelosCJ <83297899+angelosCJ@users.noreply.github.com>
2025-08-07 15:24:39 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors({origin:"*"})); // This allows requests from all origins
 
 app.use(express.json());
 
-mongoose.connect(MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB error:', err));
 
